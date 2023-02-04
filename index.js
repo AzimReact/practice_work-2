a47ee0fff7fd3e897f536c0f9217f96982f011af
Azim <azimreact@gmail.com>
2023-02-05 00:58:28 +0600

    practice_work-3

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '')
-// console.log(numberOfFilms);
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '')
+
+    while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)){
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '') 
+    }
+}
+start();
 
 const PersonalMovieDB = {
     count: numberOfFilms,
@@ -9,31 +17,48 @@ const PersonalMovieDB = {
     privat: false
 }
 
-if (PersonalMovieDB.count < 10) {
-    console.log('malo');
-} else if (PersonalMovieDB.count > 10 && PersonalMovieDB < 30) {
-    console.log('lubitel');
-} else if (PersonalMovieDB.count > 30) {
-    console.log('kinoman');
-}  else {
-    console.log('oshibka');
-}
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
 
-for (let i = 0; i < 2; i++) {
-
-    let a = prompt('Какой последний фильм ты посмотрел?', '')
-    let b = +prompt('На сколько ты его оценишь?', '')
-
-    if(a != null && b != null && a != '' && b != '' && a.length < 50){
-
-        PersonalMovieDB.movies[a] = b
-        console.log('done');
-    } else{
-
-        i--
-        console.log("error");
+        let a = prompt('Какой последний фильм ты посмотрел?', '')
+        let b = +prompt('На сколько ты его оценишь?', '')
+    
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            PersonalMovieDB.movies[a] = b
+            console.log('done');
+        } else {
+            i--
+            console.log("error");
+        }
     }
 }
+rememberMyFilms();
 
-console.log(PersonalMovieDB);
+function detectPersonalLevel() {
+    if (PersonalMovieDB.count < 10) {
+        console.log('malo');
+    } else if (PersonalMovieDB.count >= 10 && PersonalMovieDB.count <= 30) {
+        console.log('lubitel');
+    } else if (PersonalMovieDB.count > 30) {
+        console.log('kinoman');
+    }  else {
+        console.log('oshibka');
+    }    
+}
+detectPersonalLevel();
+
+function ShowMyDB(hidden) {
+    if (!hidden) {
+        console.log(PersonalMovieDB);
+    }
+}
+ShowMyDB(PersonalMovieDB.privat);
+
+function writeYourGenres() {
+    for (let i = 0; i < 3;  i++) {
+        PersonalMovieDB.genres[i] = prompt(`Which your lovely genre by number ${ i+1 }?`, '')
+    }
+}
+writeYourGenres();
+
 
